ea47feee8e2568aa0ed8003ea009e95eca45d590
Md Miskatur Rahman <59787966+sajjatflutterdeveloper@users.noreply.github.com>
2022-08-21 12:18:01 -0700

    author model

--- author/models.py
+++ author/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Author(models.Model):
     name=models.CharField(max_length=255)
-    
+    create_at=models.DateTimeField(auto_now_add=True)
     def __str__(self):
         pass
 
