932a2303be8a5459d5558b99639f234610a65c71
Md Miskatur Rahman <59787966+sajjatflutterdeveloper@users.noreply.github.com>
2022-08-21 12:10:19 -0700

    create author app

--- course/models.py
+++ course/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Course(models.Model):
+    title=models.CharField(max_length=255)
+    content=models.TextField()
+    create_at=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        db_table = 'course'
+        managed = True
+     
