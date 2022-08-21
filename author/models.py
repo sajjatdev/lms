7419329b36a741588acf40ed251c61af559142f0
Md Miskatur Rahman <59787966+sajjatflutterdeveloper@users.noreply.github.com>
2022-08-21 12:17:19 -0700

    author model create

--- author/models.py
+++ author/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+    name=models.CharField(max_length=255)
+    
+    def __str__(self):
+        pass
+
+    class Meta:
+        db_table = 'author'
+        managed = True
+        verbose_name = 'Author'
+        verbose_name_plural = 'Authors'
