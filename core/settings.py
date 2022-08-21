6dd0e7a1f52bd70b69ae38fb02808e00947d30f3
Md Miskatur Rahman <59787966+sajjatflutterdeveloper@users.noreply.github.com>
2022-08-21 11:59:41 -0700

    MySql Setup

--- core/settings.py
+++ core/settings.py
@@ -66,11 +66,14 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'course',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
